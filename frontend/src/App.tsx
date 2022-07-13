dd31ffa42341d819f292131498701fe9a0647b48
Letícia Félix <leticiafelix2799@gmail.com>
2022-07-13 17:14:57 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
-import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
-import SalesCard from "./components/SalesCard"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
@@ -17,4 +16,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
