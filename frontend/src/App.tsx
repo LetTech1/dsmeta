72b98e7ab0a99dd82d25db10e2e08d81bf5e9476
Letícia Félix <leticiafelix2799@gmail.com>
2022-07-13 16:26:39 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta"/>
+                    <h1>DSMeta</h1>
+                    <p>Desenvolvido por
+                        <a href="https://github.com/LeticiaBapFelix"> Letícia Félix</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
+
