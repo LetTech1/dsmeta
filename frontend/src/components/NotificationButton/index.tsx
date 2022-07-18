efbee34186fd71044bfdaf6244a11a01b523eae8
Letícia Félix <leticiafelix2799@gmail.com>
2022-07-18 20:26:01 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,24 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../Utils/request';
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("Sucesso");
+        });
+}
+
+function NotificationButton({ saleId }: Props) {
     return (
-    <div className="dsmeta-button-notification">
-        <img src={icon} alt="Notificar" />
-    </div>)
+        <div className="dsmeta-button-notification" onClick={() => handleClick(saleId)}>
+            <img src={icon} alt="Notificar" />
+        </div>)
 }
 
 export default NotificationButton;
