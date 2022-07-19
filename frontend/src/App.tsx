466b40b8cacd9227ae11ababb9fa5e6b0043c9cd
Letícia Félix <leticiafelix2799@gmail.com>
2022-07-19 12:57:45 -0300

    Final adjustment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import Header from "./components/Header";
-import SalesCard from "./components/SalesCard";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
